27ea595104c1b285b6824465735c6aae9ec5412c
Paul Ledin <paul_ledin@uwalumni.com>
2025-05-19 12:10:37 -0500

    Update Monthly_Change_Reports.py

--- Monthly_Change_Reports.py
+++ Monthly_Change_Reports.py
@@ -407,13 +407,9 @@ else:
         st.markdown('*Liquidations:* ' + '**' + str(len(df_liquidated)-1) + '**')
         st.markdown('---')
     
-        #df_cuna_reafl_chgs = getAFLChgsTables(selected_month, 'REAFL', 'cuna')
         df_cuna_reafl_chgs = getAFLChgsTableFromDB(selected_month, 'REAFL', 'cuna')
         df_nafcu_reafl_chgs = getAFLChgsTableFromDB(selected_month, 'REAFL', 'nafcu')
         df_either_reafl_chgs = getAFLChgsTableFromDB(selected_month, 'REAFL', 'either')
-
-        #df_nafcu_reafl_chgs = getAFLChgsTables(selected_month, 'REAFL', 'nafcu')
-        #df_either_reafl_chgs = getAFLChgsTables(selected_month, 'REAFL', 'either')
     
         st.markdown('**Reaffiliations**')
         st.markdown('*Reaffiliations:* ' + '**' + str(len(df_cuna_reafl_chgs)-1) + '**')
