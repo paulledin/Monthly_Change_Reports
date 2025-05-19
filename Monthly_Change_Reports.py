e81101c2dfc2a92006e24b43705871d4fa426cc6
Paul Ledin <paul_ledin@uwalumni.com>
2025-05-19 12:05:40 -0500

    Update Monthly_Change_Reports.py

--- Monthly_Change_Reports.py
+++ Monthly_Change_Reports.py
@@ -275,10 +275,9 @@ def getAFLChgsTableFromDB(month, chg_type, afl_type):
     sqlStmt = "SELECT * FROM monthly_report.change_reports."
     
     if(chg_type == 'REAFL'):
-        #sqlStmt =+ "reafl_chgs_" + afl_type + "_" + convertDateToSystem(month)
-        sqlStmt =+ "reafl_chgs_" 
+        sqlStmt += "reafl_chgs_" + afl_type + "_" + convertDateToSystem(month) 
     elif(chg_type == 'DISAFL'):
-        sqlStmt =+ "disafl_chgs_" + afl_type + "_" + convertDateToSystem(month)
+        sqlStmt += "disafl_chgs_" + afl_type + "_" + convertDateToSystem(month) 
 
     st.write(sqlStmt)
 
