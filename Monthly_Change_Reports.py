798fc86eb222818dd38989c7539fe721bb5964cd
Paul Ledin <paul_ledin@uwalumni.com>
2025-05-19 11:01:15 -0500

    Update Monthly_Change_Reports.py

--- Monthly_Change_Reports.py
+++ Monthly_Change_Reports.py
@@ -334,9 +334,11 @@ else:
         selected_month = st.selectbox('Month', month)
 
     df_afl_table_cuna = getTableAFLTable_from_db(selected_month, 'cuna')
-    
+    df_afl_table_nafcu = getTableAFLTable_from_db(selected_month, 'nafcu')
+
+
     #df_afl_table_cuna = getAFLTable(selected_month, 'cuna')  
-    df_afl_table_nafcu = getAFLTable(selected_month, 'nafcu')
+    #df_afl_table_nafcu = getAFLTable(selected_month, 'nafcu')
     df_afl_table_either = getAFLTable(selected_month, 'either')    
     
     df_mergers = getMergersTable(selected_month)
