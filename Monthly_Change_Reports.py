a527eeb31c513c4499d69682fec835ef99808361
Paul Ledin <paul_ledin@uwalumni.com>
2025-05-19 11:37:51 -0500

    Update Monthly_Change_Reports.py

--- Monthly_Change_Reports.py
+++ Monthly_Change_Reports.py
@@ -378,9 +378,9 @@ else:
         st.markdown('---')
         st.markdown('**Month Ended :: ' + selected_month + '**')
         st.markdown('*Active Credit Unions:* ' + '**' + str(format_number(df_afl_table_cuna.iloc[len(df_afl_table_cuna) - 1, 5])) + '**')
-        st.markdown('*Affiliated (CUNA):* ' + '**' + str(format_number(df_afl_table_cuna.iloc[len(df_afl_table_cuna) - 1, 1])) + '**')
-        st.markdown('*Affiliated (NAFCU):* ' + '**' + str(format_number(df_afl_table_nafcu.iloc[len(df_afl_table_nafcu) - 1, 1]))  + '**')
-        st.markdown('*Affiliated (Either):* ' + '**' + str(format_number(df_afl_table_either.iloc[len(df_afl_table_either) - 1, 1])) + '**')
+        st.markdown('*Affiliated with America\'s CUs:* ' + '**' + str(format_number(df_afl_table_cuna.iloc[len(df_afl_table_cuna) - 1, 1])) + '**')
+        #st.markdown('*Affiliated (NAFCU):* ' + '**' + str(format_number(df_afl_table_nafcu.iloc[len(df_afl_table_nafcu) - 1, 1]))  + '**')
+        #st.markdown('*Affiliated (Either):* ' + '**' + str(format_number(df_afl_table_either.iloc[len(df_afl_table_either) - 1, 1])) + '**')
         st.markdown('---')
     
         st.markdown('**Affiliation Ratios**')
