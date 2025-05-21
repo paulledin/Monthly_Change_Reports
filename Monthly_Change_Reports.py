29632ac053e48cc2003b48fcdb733c0b54425807
Paul Ledin <paul_ledin@uwalumni.com>
2025-05-21 15:14:25 -0500

    Update Monthly_Change_Reports.py

--- Monthly_Change_Reports.py
+++ Monthly_Change_Reports.py
@@ -361,13 +361,6 @@ else:
         max_value=1000000000000,
         step=1,
         format="localized",),
-        #"% CUs Affiliated": st.column_config.NumberColumn(
-        #"CUs Affiliated (%)",
-        #help="Percentage of CUs Affiliated with ACUs",
-        #min_value=0,
-        #max_value=100,
-        #step=1,
-        #format="%.2f",)
         }
 
     '''
