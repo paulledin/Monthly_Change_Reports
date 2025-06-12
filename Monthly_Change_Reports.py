771ff52ff87cc74273aa39aac2d588121f4d0887
Paul Ledin <paul_ledin@uwalumni.com>
2025-06-12 15:58:15 -0500

    Update Monthly_Change_Reports.py

--- Monthly_Change_Reports.py
+++ Monthly_Change_Reports.py
@@ -18,7 +18,7 @@ st.set_page_config(
     initial_sidebar_state="expanded")
 
 thePassPhrase = st.secrets["thePassPhrase"]
-dbConn = st.connection("snowflake")
+#dbConn = st.connection("snowflake")
 
 ###############################################################################
 #Function Definitions
